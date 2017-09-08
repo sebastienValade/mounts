1c6a3ec46713cc6e7468eabde3480c4ce7e8f1b2
Sebastien Valade <sebastien.valade@tu-berlin.de>
2017-09-08 20:29:50 +0200

    major updates

--- src/snippet_fetchme.py
+++ src/snippet_fetchme.py
@@ -25,15 +25,26 @@ logging.info('>> script started')
 # --- scihub query
 # productlist = obj.scihub_search(print_url=1)
 
-# --- search specific file
+# --- parse options from kwargs
+# obj.scihub_search(filename='S1*')
+# obj.scihub_search(footprint='POLYGON((-2.211 49.9654, -0.835 49.9654, -0.835 49.1206, -2.211 49.1206, -2.211 49.9654))')
+
+# --- parse options from dict
+# optns = {'filename': 'S1A*', 'maxrecords': 5}
+# obj.scihub_search(**optns)
+
+# --- parse options from yaml configuration file
+# obj.scihub_search(configfile='./conf/_config_ertaale.yml')
+
+# -- parse options from loaded yaml configration file
+# obj.read_configfile('./conf/_config_ertaale.yml')
+# obj.scihub_search()
+
+# ex: search specific file
 # productlist = obj.scihub_search(filename='S1A_IW_SLC__1SDV_20170507T050422_20170507T050449_016471_01B4A7_9DF7*')
 
-# --- query/download using options in config file
-# obj.scihub_search(configfile='./conf/_config_etna_archive.yml')
+# ex: print url, export results as xml
 # obj.scihub_search(configfile='./conf/_config_pitonfournaise_archive.yml', print_url=1, export_result=1)
-
-# --- query using WKT footprint, filename, ...
-# obj.scihub_search(footprint='POLYGON((-2.211 49.9654, -0.835 49.9654, -0.835 49.1206, -2.211 49.1206, -2.211 49.9654))', filename='S1*_SLC*')
 
 
 # =============================================
